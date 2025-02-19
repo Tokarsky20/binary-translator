1303198544cf1338e534ed7755ef724a1f7115e3
oyyynik <oyyynik@gmail.com>
2025-02-19 19:38:08 +0200

    I added a code that converts text to bits.

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 def text_to_binary(text):
     binary_values = []
     for char in text:
-        if char == ' ':
-            binary_values.append('100000')
-        else:
-            binary_values.append(format(ord(char), '07b'))
+        binary_values.append(format(ord(char), '08b'))
     return ' '.join(binary_values)
 
-if __name__ == '__main__':
-    message = "Text"
-    binary_message = text_to_binary(message)
-    print(binary_message)
+
+def binary_to_text(binary_text):
+    text_values = []
+    binary_chars = binary_text.split()
+
+    for binary_char in binary_chars:
+        try:
+            text_values.append(chr(int(binary_char, 2)))
+        except ValueError:
+            return "Помилка: Невірний формат бінарного коду!"
+
+    return ''.join(text_values)
+
+
+if __name__ == "__main__":
+    choice = input("Оберіть режим (1 - текст у бінарний, 2 - бінарний у текст): ")
+
+    if choice == "1":
+        message = input("Введіть текст: ")
+        binary_message = text_to_binary(message)
+        print(f"Бінарний код: {binary_message}")
+
+    elif choice == "2":
+        binary_message = input("Введіть бінарний код: ")
+        decoded_text = binary_to_text(binary_message)
+        print(f"Розшифрований текст: {decoded_text}")
+
+    else:
+        print("Невірний вибір!")
